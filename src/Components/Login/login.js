00f650d4f41d7281d2b914e7cc73dc033b105657
sarawarnock <sara@sarawarnock.com>
2020-11-29 11:39:56 -0700

    start login code

--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,1 +1,4 @@
 //generate a call to the /authorize endpoint for the web API
+//to use the Web API, need to authorize with OAuth 2.0
+//NOTE: OAuth is NOT an authentication protocol in itself, it is used INSIDE of authentical protocosl, 
+//
